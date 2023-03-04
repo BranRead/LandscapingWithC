ebd68baca78f20fd5177de114d3f07f57282daed
BranRead <branrtread@gmail.com>
2023-03-04 12:40:29 -0400

    Takes in all inputs needed. Need to make conditional statement for grass.

--- main.c
+++ main.c
@@ -1,6 +1,44 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+
+    int houseNum, length, width, numOfTrees;
+    char grass[9], blank;
+    float purchasePrice();
+
+    printf("Enter House Number: ");
+    scanf("%d", &houseNum);
+    printf("\n");
+
+    printf("Enter property length (feet): ");
+    scanf("%d", &length);
+    printf("\n");
+
+    printf("Enter property width (feet): ");
+    scanf("%d", &width);
+    scanf("%c", &blank);
+    printf("\n");
+
+    printf("Enter type of grass (fescue, bentgrass, campus): ");
+    fgets(grass, 9, stdin);
+    printf("\n");
+
+    printf("Enter the number of trees: ");
+    scanf("%d", &numOfTrees);
+    printf("\n");
+
+    printf("Total cost for house %d is: $%.2f\n", houseNum);
+
     return 0;
+}
+
+float purchasePrice(int length, int width, char grass[], int numOfTrees){
+    int baseLabour = 1000;
+    int area = length * width;
+
+    if((length * width) > 5000){
+        baseLabour += 500;
+    };
+
+    if(grass == "fescue")
 }
